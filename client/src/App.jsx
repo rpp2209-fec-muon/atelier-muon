f4f1316e0596a488f8318f82381d49e5c3a56396
San Chui <chuisan3021@gmail.com>
2023-01-12 20:58:00 -0800

    Setup Webpack and Babel, connect to the  basic file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { // useState( setState ==> this.setState {})
+
+    }
+  }
+
+  componentDidMount() { // useEffect
+
+  }
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default App;
