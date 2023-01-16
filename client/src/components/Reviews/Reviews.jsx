d29e21a57f98b40fa7b62ec4346429732d0854bc
CJ Edgecombe <cjledgecombe@gmail.com>
2023-01-16 11:30:21 -0700

    added initial list of component files for the Reviews widget

---
+++ client/src/components/Reviews/Reviews.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Reviews() {
+
+  return (
+    <div>Ratings and Reviews</div>
+  )
+}
---
+++ client/src/components/Reviews/Tile.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Tile(props) {
+
+  return (
+    <div></div>
+  )
+}
