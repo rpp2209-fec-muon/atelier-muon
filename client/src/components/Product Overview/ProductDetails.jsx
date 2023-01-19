e3e85fdd7bd1fd0f7ea4f8bcfc3d19faa0f1f019
San Chui <chuisan3021@gmail.com>
2023-01-18 23:34:20 -0800

    Change back  the changes made on Product Overview

--- client/src/components/Product Overview/ProductDetails.jsx
+++ client/src/components/Product Overview/ProductDetails.jsx
@@ -1,18 +1,18 @@
-// // <!-- This is just a placeholder so git commits the basic folder structure! Feel free to delete me when you start adding files to this folder. -->
-// import React, {useEffect, useState } from 'react';
-// import ImageGallery from './Overview Components/Image Gallery.jsx';
-// import ProductInfo from './Overview Components/Product Information.jsx';
-// import StyleSelector from './Overview Components/Style Selector.jsx';
-// import AddToCart from './Overview Components/Add Cart.jsx';
+// <!-- This is just a placeholder so git commits the basic folder structure! Feel free to delete me when you start adding files to this folder. -->
+import React, {useEffect, useState } from 'react';
+import ImageGallery from './Overview Components/Image Gallery.jsx';
+import ProductInfo from './Overview Components/Product Information.jsx';
+import StyleSelector from './Overview Components/Style Selector.jsx';
+import AddToCart from './Overview Components/Add Cart.jsx';
 
-// function ProductDetailsPage () {
+function ProductDetailsPage () {
 
-//   return ([
-//     <ImageGallery />,
-//     <ProductInfo />,
-//     <StyleSelector />,
-//     <AddToCart />,
-//   ])
-// }
+  return ([
+    <ImageGallery />,
+    <ProductInfo />,
+    <StyleSelector />,
+    <AddToCart />,
+  ])
+}
 
-// export default ProductDetailsPage;
+export default ProductDetailsPage;
