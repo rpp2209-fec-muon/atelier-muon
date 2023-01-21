be72b222e12c1da2fc37db204735f9c3fa240326
Tiger Hong <tigerhong186@gmail.com>
2023-01-20 16:26:33 -0800

    Sub components created

---
+++ client/src/components/Product Overview/Overview Components/Add Cart.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function AddToCart () {
+
+  return ([
+    <button> Add to Cart </button>
+  ])
+}
+
+export default AddToCart;
---
+++ client/src/components/Product Overview/Overview Components/Style Selector.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function StyleSelector () {
+
+  return ([
+    <div> Style Selector </div>
+  ])
+}
+
+export default StyleSelector
