5c5c6c4579faa43a3c77e29c5957c5c1d818e64f
San Chui <chuisan3021@gmail.com>
2023-01-30 22:03:46 -0800

    completed Add outfit button

--- client/src/components/Related Products/RelatedProducts.jsx
+++ client/src/components/Related Products/RelatedProducts.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import axios from 'axios';
 import Cards from './components/Cards.jsx';
+import AddOutfit from './components/AddOutfit.jsx';
 
 class RelatedProducts extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      related: []
+      related: [],
+      outfit: [],
+      r_page: 0,
+      o_page: 0
     }
   }
   // related list: GET /products/:product_id/related
   componentDidMount() {
-    console.log("***", this.props.product_id);
-    axios
-      .get('/products', { params: { type: '/related', product_id: this.props.product_id, params: {} }})
-      .then((data) => {
-        var newData = this.uniqueArray(data.data);
+    this.getRelatedList();
+    this.getOutfitList();
+  }
 
-        console.log(newData);
-        this.setState({
-          related: newData
-        })
+  getRelatedList() {
+    axios
+    .get('/products', { params: { type: '/related', product_id: this.props.product_id, params: {} }})
+    .then((data) => {
+      var newData = this.uniqueArray(data.data);
+
+      console.log(newData);
+      this.setState({
+        related: newData
       })
-      .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
   }
 
   uniqueArray(array) {
@@ -39,12 +47,56 @@ class RelatedProducts extends React.Component {
     return result;
   }
 
+  addToOutfit() {
+    var id = this.props.product_id.slice(1);
+    id = parseInt(id);
+    var getdata = localStorage.getItem('outfit');
+    if (getdata !== "undefined") {
+      var outfit = JSON.parse(getdata);
+    } else {
+      var outfit = null;
+    }
+    if (outfit === null) {
+      var list = [];
+      list.push(id);
+      localStorage.setItem('outfit', JSON.stringify(list));
+    } else {
+      if(outfit.indexOf(id) === -1) {
+        outfit.push(id);
+        localStorage.setItem('outfit', JSON.stringify(outfit));
+      }
+    }
+  }
+
+  getOutfitList() {
+    var getdata = localStorage.getItem('outfit');
+    if (getdata !== "undefined") {
+      var outfit = JSON.parse(getdata);
+    } else {
+      var outfit = [];
+    }
+    console.log("outfit", outfit);
+    this.setState({
+      outfit: outfit
+    })
+  }
+
   render () {
+    var r = this.state.r_page * 4;
+    var o = this.state.o_page * 3;
+    const relatedList = this.state.related.slice(r, r + 4);
+    const outfitList = this.state.outfit.slice(o, o + 3);
     return(
-      <div className ="rp-related" data-testid="rp-related">
+      <div className="rp-related" data-testid="rp-related">
         <h3>Related Products</h3>
-        <div className="rp-cards-list">{this.state.related.map((product) => <Cards product_id={product} key={product} onClick={this.props.onClick}/>)}</div>
-        <h3>My Outfits</h3>
+        <div className="rp-cards-list">
+          {relatedList.map((product) => <Cards product_id={product} key={product} onClick={this.props.onClick} kind={'r'}/>)}
+        </div>
+        <h3>Your Outfit</h3>
+          <div className="rp-outfit-list">
+            <AddOutfit onClick={this.addToOutfit.bind(this)}/>
+            {outfitList.map((product) => <Cards product_id={product} key={product} onClick={this.props.onClick} kind={'o'}/>)}
+          </div>
       </div>
     )
   }
