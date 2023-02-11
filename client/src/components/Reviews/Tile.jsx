3d9608a72d93fec2f56f7fd57b058c21bee51906
CJ Edgecombe <cjledgecombe@gmail.com>
2023-02-11 10:56:48 -0700

    added review tile helpfulness count and scroll functionality

--- client/src/components/Reviews/Tile.jsx
+++ client/src/components/Reviews/Tile.jsx
@@ -11,11 +11,16 @@ export default function Tile({ review }) {
     <div data-testid="tile-instance" className='tile-instance'>
       <Star ratings={review.rating}/>
       <div className='review-date'>{date}</div>
-      <div><b>{review.summary}</b></div>
-      <div>{review.body}</div>
+      <div className='review-summary'><b>{review.summary}</b></div>
+      <div className='review-body'>{review.body}</div>
       {review.recommend &&
-        <div><i>I recommend this product</i></div>
+        <div className='review-recommend'><i>I recommend this product</i></div>
       }
+      <div className='review-name'>{review.reviewer_name}</div>
+      <div className='review-helpful-parent'>
+        <div className='review-helpful'>Helpful?</div>
+        <div className='review-helpful-yes'>Yes {review.helpfulness}</div>
+      </div>
     </div>
   )
 }
